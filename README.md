c5330e896b386932f5b1be2dc2784c78b850814f
Raiicon <orankai@gmail.com>
2021-01-03 01:13:56 -0500

    open or closed sign added

---
+++ README.md
@@ -0,0 +1,11 @@
+##Barbershop Website
+
+#Barbershop pitch for Lucho Barbers. The goal of this project is to provide quick information to clients, in a visualy desirable manner. 
+
+#*Things to Include
+    *Business Hours and a front and center indicator for current status
+    *Price list
+    *About section
+    *Map. Maybe google maps api?
+    *Way to set appointments.
+    
--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const oocEl = document.getElementById('open-or-closed')
+
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('nav');
 
 toggle.addEventListener('click', 
     ()=>nav.classList.toggle('active'))
+
+
+//Our time function will determine if the shop displays open or closed
+function timeFunc() {
+    var date = new Date();
+    var hour = date.getHours();
+
+    if(hour < 7 || hour > 18) {
+        oocEl.innerHTML = `We are open now, come on in!`
+    } else {
+        oocEl.innerHTML = `We are open now, come on in!`
+    }
+
+}
+timeFunc()
